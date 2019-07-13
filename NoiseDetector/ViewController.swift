9e318a54d20af7137757c2b5e2ece757b70f496e
Frank Cheng <ufo22940268@gmail.com>
2019-07-13 14:46:43 +0800

    v

--- NoiseDetector/ViewController.swift
+++ NoiseDetector/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var toolbar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        toolbar.setShadowImage(UIImage(), forToolbarPosition: .topAttached)
     }
 
 
